a95fecdfc717e9ea042e6c425b1974fe1a38885e
Lachlan Priest <lachlanpriest@gmail.com>
2013-03-23 14:15:05 +1300

    generates audio

--- app/models/haiku.rb
+++ app/models/haiku.rb
@@ -1,3 +1,9 @@
 class Haiku < ActiveRecord::Base
   attr_accessible :description, :author
+
+  after_save :create_audio_file
+
+  def create_audio_file
+    `say "#{self.description}" -o #{Rails.root + 'public/haiku_audio' + self.id.to_s }.aiff`
+  end
 end
