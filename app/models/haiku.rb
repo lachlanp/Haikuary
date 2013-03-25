2a87c449a8ecaf150839bc47f8a41782732d6ffb
Ryan Bigg <radarlistener@gmail.com>
2013-03-25 23:47:00 +1300

    Make well_formed kind of work (when you want it to)

--- app/models/haiku.rb
+++ app/models/haiku.rb
@@ -1,6 +1,6 @@
 class Haiku < ActiveRecord::Base
   validate :word_count_less_than_18
-  validate :well_formed
+  #validate :well_formed
   after_save :create_audio_file
 
   def create_audio_file
@@ -9,6 +9,12 @@ class Haiku < ActiveRecord::Base
     `say "#{self.description}" -o #{local_path}`
     if File.size(local_path) > 10000
       self.update_column(:file_path, possible_path)
+    end
+  end
+
+  def dissect
+    description.split("\n").each_with_index.map do |line, index|
+      dissect_line(line)
     end
   end
 
@@ -24,13 +30,18 @@ class Haiku < ActiveRecord::Base
     word.sub!(/^y/, '')
     word.scan(/[aeiouy]{1,2}/).size
   end
-  
+
+  def dissect_line(line)
+    line.split(" ").map do |w|
+      word_check(w.gsub(/[^\w+\s]/, ''))
+    end
+  end
+
   def well_formed
     framing = [5,7,5]
-    haiku.split("\n").each_with_index do |line, index|
-      count = line.split(" ").map do |w|
-      word_check(w.gsub(/[^\w+\s]/, ''))
-    end.inject(&:+)
-    errors[:widget] << "is invalid on line #{index+1}" if framing[index] != count
+    description.split("\n").each_with_index do |line, index|
+      count = dissect_line(line).inject(&:+)
+      errors[:widget] << "is invalid on line #{index+1}" if framing[index] != count
+    end
   end
 end
