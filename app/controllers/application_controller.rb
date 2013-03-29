fab01ec0bb1eec84d1faaf56bae0e211a4d107a3
Lachlan Priest <lachlanpriest@gmail.com>
2013-03-30 00:15:56 +1300

    redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_filter :ensure_domain
   protect_from_forgery
+
+  APP_DOMAIN = 'www.haikudetat.com'
+  def ensure_domain
+    if Rails.env == 'production' && request.env['HTTP_HOST'] != APP_DOMAIN
+      redirect_to "http://#{APP_DOMAIN}#{request.path}", :status => 301
+  end
 end
