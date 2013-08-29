e1e6df0db2c6a4d9c2ff9d917be224e8c2443919
Lachlan Priest <lachlanpriest@gmail.com>
2013-08-30 11:39:09 +1200

    new rake task

---
+++ lib/tasks/randomtweet.rb
@@ -0,0 +1,9 @@
+
+namespace :twitter do
+  desc "Daily random tweet"
+  task post_tweet: :environment do
+    haiku = HaikuMaker.new.generate
+    haiku.save
+    Twitter.update(haiku.description)
+  end
+end
