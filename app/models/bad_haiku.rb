0f0ce1ff3c869b19d280115b51cebbb7f6ff6167
Lachlan Priest <lachlanpriest@gmail.com>
2013-09-20 14:52:22 +1200

    after updating badhaiku, attempts resubmit

--- app/models/bad_haiku.rb
+++ app/models/bad_haiku.rb
@@ -1,8 +1,14 @@
 class BadHaiku < ActiveRecord::Base
   before_save :how_many_syllables
+  after_save :attempt_resubmit
 
   def how_many_syllables
     self.syllable_estimate = SyllableCounter::Count.new.get_syllables(self.description)
     return true
+  end
+
+  def attempt_resubmit
+    haiku = Haiku.new(author: author, description: description)
+    self.destroy if haiku.save
   end
 end
