af878f89993c810df64e4eecdd48f122ce08c622
Lachlan Priest <lachlanpriest@gmail.com>
2014-12-14 23:07:39 +0800

    Cleanup get_random method

--- app/models/haiku.rb
+++ app/models/haiku.rb
@@ -8,17 +8,10 @@ class Haiku < ActiveRecord::Base
   scope :not_generated, -> { where.not(author: 'Happy Haiku Bot') }
 
   def self.get_random
-    Haiku.not_generated.sample
-  end
-
-  def dissect
-    description.split("\n").each_with_index.map do |line, index|
-      dissect_line(line)
-    end
-  end
-
-  def is_new_line_formatted?
-    self.description.lines.count >= 3
+    scope = Haiku.not_generated
+    random_id = rand(scope.count).to_i
+    haiku = scope.find_by(id: random_id)
+    haiku ? haiku : get_random
   end
 
 private
@@ -31,20 +24,12 @@ private
     errors[:description] << "too many words" if description.split.size > 17
   end
 
-  def word_check(word)
-    word.downcase!
-    return 1 if word.length <= 3
-    word.sub!(/(?:[^laeiouy]es|[^t]ed|[^laeiouy]e)$/, '')
-    word.sub!(/^y/, '')
-    word.scan(/[aeiouy]{1,2}/).size
-  end
-
   def dissect_line(line)
     SyllableCounter::Count.new.get_syllables(line)
   end
 
   def well_formed
-    description.split("\n").each_with_index do |line, index|
+    description.lines.each_with_index do |line, index|
       count = dissect_line(line)
       if ((valid_syllables_for_line(index) - count).abs > 1)
         errors[:description] << "wrong number of syllables on line #{index+1}"
@@ -53,8 +38,7 @@ private
   end
 
   def formation
-    errors[:description] << "Please use 3 lines." unless description.lines.count >= 3
-    errors[:description] << "Please use only 3 lines." unless description.lines.count <= 3
+    errors[:description] << "Please use 3 lines." unless description.lines.count == 3
   end
 
   def valid_syllables_for_line(index)
